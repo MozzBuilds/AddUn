b56253d7f92a9834fcf6afb986175ee3aeb48278
Colin Morrison <colinjmmorris@gmail.com>
2021-09-17 21:33:36 +0100

    Labels hooked to actions. Timer and user enter not working yet

--- AddUn/GameViewController.swift
+++ AddUn/GameViewController.swift
@@ -20,15 +20,17 @@ class GameViewController: UIViewController {
 
     
     //Initialize the values that feed the variables
-    var userInputText = ""; //String
+//    var userInputText = ""; //String
     var score = 0; //Int
     var timer:Timer? //Timer object
-    var timerSeconds = 60 //Initial value for the number of seconds
+    let timerSetting = 60
+    var timerSeconds = 0 //Initial value for the number of seconds
     //None for num, since this doesn't need used
-    
     
     //We want 5 numbers to be generated, so range is index 0 to 4. Dealing with ints, so using <5 will get us range indexes 0,1,2,3,4 fine
     let numberQuantity = 0..<5
+    
+    
     
     func numberGenerator()-> String{
         
@@ -60,7 +62,7 @@ class GameViewController: UIViewController {
     
     //Now need setter functions for our IBOutlets, according to the 'num','userInput', 'time', and 'score' vars declared above
     func setUserInputField(){
-        
+        userInputField?.text = ""
     }
     
     func setNumLabel(){
@@ -75,12 +77,14 @@ class GameViewController: UIViewController {
         timerLabel?.text = String(format: "%d", timerSeconds) + "s"
     }
     
+    
     //When the view has loaded, run our setters to set our initial values
     override func viewDidLoad() {
         super.viewDidLoad()
         setUserInputField()
         setNumLabel()
         setScoreLabel()
+        timerSeconds = timerSetting //Initialize timerSeconds at 60
         setTimerLabel()
     }
     
@@ -93,13 +97,16 @@ class GameViewController: UIViewController {
 //
 //        }
         
-        
+        //If either the number or user input is nil, return for now
         if (numLabel == nil || userInputField == nil) {
             return
+        
         } else {
+            //Else, if the user has not yet input the correct quantity of numbers
             if (userInputField?.text?.count != numberQuantity.count){
                 return
             } else {
+                //Else if all numbers have been filled in
                 checkAnswer()
             }
         }
@@ -122,12 +129,15 @@ class GameViewController: UIViewController {
         return s[index].wholeNumberValue ?? 0
     }
     
+    
+    
     func checkAnswer(){
+        
+        //Set initial status of whether the user is correct or not. If we assumed false, we'd have to check ALL numbers before confirming true. But by assuming false, we can catch it earlier on, maybe
+        var correctStatus = true
 
         //At each position of i, for each number in our quantity of numbers range
         for i in numberQuantity{
-            
-            var correctStatus = true
             
             //Get the int the user has put in
             var userInput = findInt(at: userInputField?.text ?? "", at: i)
@@ -139,14 +149,54 @@ class GameViewController: UIViewController {
 //                userInput = 10
 //            }
             
-            //Shorthand?
+            //Shorthand, might work
             userInput == 0 ? userInput = 10 : nil
+            
+//            userInput != (targetNumber + 1) ? correctStatus = false;break : nil
             
             if (userInput != targetNumber + 1){
                 correctStatus = false
                 break //Stop here since this round is lost
             }
+        }
+        
+        //By this point we know if user was correct or not, via correctStatus
+        //Update the score, giving 5 points
+        ////Down the line, I want there to be higher points for getting two or more in a row
+        correctStatus ? score += 5 : nil
+        
+        //After answer is checked, we want to set our labels again, check the timer, and finish the game if necessary
+        setNumLabel()
+        setScoreLabel()
+        setUserInputField()
+        checkTimer()
     }
+    
+    
+    func checkTimer(){
+        if (timer == nil){
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ timer in
+                
+                if (self.timerSeconds == 0) {
+                    self.endGame() //Having to use self here
+                } else if (self.timerSeconds <= self.timerSetting){
+                    self.timerSeconds -= 1
+                    self.setTimerLabel()
+                }
+            }
+        }
+    }
+    
+    
+    func endGame(){
+        
+        //Reset the timer
+        //Display a message saying you got so many points
+        //Action to start again
+        //Reset the score, time, and the corresponding labels
+    }
+        
+    
     
     
     
@@ -195,4 +245,4 @@ class GameViewController: UIViewController {
 //    override var prefersStatusBarHidden: Bool {
 //        return true
 //    }
-}
+    }
